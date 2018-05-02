a132bc1fe63c45717bfe585c932cd395de212777
André Gomes <andreviniciusgsg@gmail.com>
2018-05-02 12:03:37 -0300

    Update install_gr-somac.sh

--- install_gr-somac.sh
+++ install_gr-somac.sh
@@ -70,8 +70,6 @@ cd ~
 git clone https://github.com/andreviniciusgsg/gr-somac.git
 cd gr-somac
 cd examples
-grcc ./gaussian_traffic_gen.grc
-grcc ./data_link_trafficgen.grc
 cd ..
 mkdir build
 cd build
@@ -81,6 +79,8 @@ sudo ldconfig
 
 cd ../examples
 grcc ./data_link.grc
+grcc ./gaussian_traffic_gen.grc
+grcc ./data_link_trafficgen.grc
 
 echo ""
 echo "Done! You should be able to use gr-macprotocols based on gr-ieee802-11 right now. Please, report any problems <andre.gomes@dcc.ufmg.br>."
