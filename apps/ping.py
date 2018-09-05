e1c4b62f630f708984cf2490e1899083e223cf69
André Gomes <andre.gomes@dcc.ufmg.br>
2018-09-05 20:17:16 -0300

    change ping interval to 0.1 s

--- apps/ping.py
+++ apps/ping.py
@@ -13,6 +13,6 @@ sleep(time_list[t_id])
 
 print("Running time = {} min".format(time_list[n - t_id] / 60))
 f = open("/dev/null", "w")
-call(["sudo", "timeout", str(time_list[n - t_id] + 1800), "ping", "-i 0.05", "192.168.123.1"], stdout = f)
+call(["sudo", "timeout", str(time_list[n - t_id] + 1800), "ping", "-i 0.1", "192.168.123.1"], stdout = f)
 
 print("Done!")
