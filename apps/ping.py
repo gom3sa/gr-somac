844d839cf205dbeb6ebadef24f7fe7322d29c5f7
André Gomes <andre.gomes@dcc.ufmg.br>
2018-09-06 12:46:33 -0300

    edit ping, adding 2nd round

--- apps/ping.py
+++ apps/ping.py
@@ -3,16 +3,38 @@ import numpy as np
 from time import sleep
 import sys
 
-time_list = [t for t in range(0, 16200, 360)]
+time_list = [t for t in range(0, 3240, 360)]
 n = len(time_list) - 1
 
-t_id = int(sys.argv[1]) - 1
+t_id_0 = int(sys.argv[1]) - 1
+t_id_1 = int(sys.argv[2]) - 1
 
-print("Sleeping time = {} min".format(time_list[t_id] / 60))
-sleep(time_list[t_id])
+####################
+# 1st round
+####################
 
-print("Running time = {} min".format(time_list[n - t_id] / 60))
+print("Sleeping time = {} min".format(time_list[t_id_0] / 60))
+sleep(time_list[t_id_0])
+
+print("Running time = {} min".format(time_list[n - t_id_0] / 60))
 f = open("/dev/null", "w")
-call(["sudo", "timeout", str(time_list[n - t_id] + 1800), "ping", "-i 0.1", "192.168.123.1"], stdout = f)
+call(["sudo", "timeout", str(time_list[n - t_id_0] + 1800), "ping", "-i 0.1", "192.168.123.1"], stdout = f)
+
+####################
+# Interval
+####################
+call(["sudo", "pkill", "ping"], stdout = f)
+sleep(30)
+
+####################
+# 2nd round
+####################
+print("Sleeping time = {} min".format(time_list[t_id_1] / 60))
+sleep(time_list[t_id_1])
+
+print("Running time = {} min".format(time_list[n - t_id_1] / 60))
+call(["sudo", "timeout", str(time_list[n - t_id_1] + 1800), "ping", "-i 0.1", "192.168.123.1"], stdout = f)
+
+f.close()
 
 print("Done!")
