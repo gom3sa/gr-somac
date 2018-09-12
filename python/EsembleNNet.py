fix error calculation of weights on Esemble.py

python/EsembleNNet.py
@@ -130,10 +130,10 @@ class EsembleNNet:
 
                 self.estimators_.append(e)
 
-                err = np.array([[self.nrmse(y, e.predict(_x)) for e in self.estimators_]])
-                acc = 1. - err
+            err = np.array([[self.nrmse(y, e.predict(_x)) for e in self.estimators_]])
+            acc = 1. - err
 
-                self.w = acc / np.sum(acc) if np.sum(self.w) == 0 else self.w * self.epsilon + (1. - self.epsilon) * acc / np.sum(acc)
+            self.w = acc / np.sum(acc) if np.sum(self.w) == 0 else self.w * self.epsilon + (1. - self.epsilon) * acc / np.sum(acc)
 
         else:
             print("No update. Forest is already full.")