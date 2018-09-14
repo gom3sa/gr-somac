a587d9c4da02d34fd5618c6b8aaf65cd0f74c1bd
André Gomes <andre.gomes@dcc.ufmg.br>
2018-09-14 11:07:45 -0300

    Simple voting, disable Ensemble NNets

--- python/decision.py
+++ python/decision.py
@@ -248,7 +248,6 @@ class decision(gr.basic_block):
                                 #prot, gain = nnet.decision(log_dict[t])
 				#print "Decision = {}, Current prot = {}, gain = {}%".format(prot, portid, gain * 100)
 
-
                                 print "Random Forest"
                                 prot, gain, prot_ = rf.decision(log_dict[t])
                                 print "Decision = {}, Current prot = {}, gain = {}%\t|\tprot_ = {}".format(prot, portid, gain * 100, prot_)
