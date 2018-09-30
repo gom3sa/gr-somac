186e2c3187114b2b9e178ed5fd66bf960f89ab7f
André Gomes <andre.gomes@dcc.ufmg.br>
2018-09-30 10:39:49 -0300

    add ping3

---
+++ apps/ping3.py
@@ -0,0 +1,35 @@
+from subprocess import call
+import numpy as np
+from time import sleep
+import sys
+
+slot_time = 360
+
+slots = np.array([
+		[5, 9 + 1],
+		[0, 4 + 1],
+		[1, 5 + 1],
+		[6, 9 + 1],
+		[2, 6 + 1],
+		[3, 7 + 1],
+		[7, 9 + 1],
+		[8, 9 + 1],
+		[8, 9 + 1]
+	]) * slot_time
+
+arg_id = int(sys.argv[1]) - 1
+
+####################
+# Ping
+####################
+
+print("Sleeping time = {} min".format(slots[arg_id, 0] / 60))
+sleep(slots[arg_id, 0])
+
+print("Running time = {} min".format(slots[arg_id, 1] / 60))
+f = open("/dev/null", "w")
+call(["sudo", "timeout", str(slots[arg_id, 1]), "ping", "-i 0.1", "192.168.123.1"], stdout = f)
+
+f.close()
+
+print("Done!")
