7b450d9d564ef9f36d2901ca7781df67f5f7bdbe
André Gomes <andre.gomes@dcc.ufmg.br>
2018-09-30 10:23:51 -0300

    checkpoint

--- python/QLearningBoltzmann.py
+++ python/QLearningBoltzmann.py
@@ -13,7 +13,7 @@ class QLearningBoltzmann:
 		
 		#self.q_table	= np.random.rand(2, 2) - 0.5
 		self.prob_table = np.zeros((2, 2)) + 0.5
-		self.T		    = 1
+		self.T		= 0.5
 		self.discount   = 0.8
 		self.learn_rate = 0.3
 		self.reward     = 0.
