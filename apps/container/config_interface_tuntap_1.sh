382749d03b46153b20afd4033e85c8d25719325a
André Gomes <andre.gomes@dcc.ufmg.br>
2018-10-30 23:14:40 -0300

    add script for running experiments in containers

---
+++ apps/container/config_interface_tuntap_1.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo "Configuration of tap interface."
+
+# Which interface is this?
+INTERFACE_ID="1"
+
+MAC_LIST=("12:34:56:78:90:aa" "12:34:56:78:90:ab" "12:34:56:78:90:ac" "12:34:56:78:90:ad" "12:34:56:78:90:ae" "12:34:56:78:90:af" "12:34:56:78:90:ba" "12:34:56:78:90:bb" "12:34:56:78:90:bc" "12:34:56:78:90:bd")
+IP_LIST=("192.168.123.1" "192.168.123.2" "192.168.123.3" "192.168.123.4" "192.168.123.5" "192.168.123.6" "192.168.123.7" "192.168.123.8" "192.168.123.9" "192.168.123.10");
+
+GATEWAY_MAC=${MAC_LIST[0]}
+GATEWAY_IP=${IP_LIST[0]}
+
+MAC=${MAC_LIST[$INTERFACE_ID]}
+IP=${IP_LIST[$INTERFACE_ID]}
+
+if [[ $INTERFACE_ID -eq "0" ]]; then
+	echo "Configuring Gateway interface.";
+
+	ip tuntap add dev tap0 user ${USER} mode tap; sleep 1
+
+	ifconfig tap0 down; sleep 1
+	ifconfig tap0 hw ether $MAC; sleep 1
+	ifconfig tap0 mtu 440; sleep 1
+	ifconfig tap0 up; sleep 1
+	ifconfig tap0 $IP; sleep 1
+
+	route del -net 192.168.123.0/24; sleep 1
+	route add -net 192.168.123.0/24 mss 400 dev tap0; sleep 1
+
+	tc qdisc del dev tap0 root; sleep 1
+	tc qdisc add dev tap0 root netem delay 10ms; sleep 1
+
+	# Gateway knows IPs vs MACs of all nodes on the network
+	count=1
+	while [[ $count -le ${#MAC_LIST[@]} ]]; do
+		arp -s ${IP_LIST[$count]} ${MAC_LIST[$count]}
+		count=$(( $count + 1 ));
+	done
+fi
+
+if [[ ! $INTERFACE_ID -eq "0" ]]; then 
+	echo "Configuring normal node, addr = ${MAC_LIST[$INTERFACE_ID]}."
+
+	ip tuntap add dev tap0 user ${USER} mode tap; sleep 1
+
+	ifconfig tap0 down; sleep 1
+	ifconfig tap0 hw ether $MAC; sleep 1
+	ifconfig tap0 mtu 440; sleep 1
+	ifconfig tap0 up; sleep 1
+	ifconfig tap0 $IP; sleep 1
+
+	route del -net 192.168.123.0/24; sleep 1
+	route add -net 192.168.123.0/24 mss 400 dev tap0; sleep 1
+
+	tc qdisc del dev tap0 root; sleep 1
+	tc qdisc add dev tap0 root netem delay 10ms; sleep 1
+
+	arp -s $GATEWAY_IP $GATEWAY_MAC
+fi
+
+echo ""
+echo "Configuration is done! Check for any errors above."
+echo ""
---
+++ apps/container/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ARG_ID=$1
+MODE=$2
+
+# IDs of TunTap script
+IDS_CONFIG=(1 2 3 4 5 6 7 8 9 GATEWAY)
+# IDs of the 1st round 
+IDS_1=(1 6 8 2 7 9 3 4 5)
+# IDs of the 2nd round
+IDS_2=(5 1 4 6 2 3 7 8 9)
+
+# Setup {{{
+PROT=$MODE; # 0: CSMA, 1: TDMA, 2: SOMAC
+DATE=`date +%d%m%Y_%H%M`;
+DIR=$HOME/$PROT"_"$DATE;
+mkdir $DIR;
+# }}}
+
+# Run {{{
+./gr-somac/apps/config_interface_tuntap_"${IDS_CONFIG[$ARG_ID]}".sh;
+
+pkill python; pkill ping;
+
+RUN_TIME=7000
+if [[ $ARG_ID -eq 9 ]]; then
+	echo "COORDINATOR"
+	echo $MODE > "/tmp/prot.txt"
+	rm -rf "/tmp/backlog_file.npy"
+	rm -rf "/tmp/out.log"
+	((sleep $RUN_TIME; pkill python) & gr-somac/examples/wifi_transceiver_FlexDataLink_"${IDS_CONFIG[$ARG_ID]}".py);
+else
+	echo "NORMAL NODE"
+	((sleep $RUN_TIME; pkill python; pkill ping) & gr-somac/examples/wifi_transceiver_FlexDataLink_"${IDS_CONFIG[$ARG_ID]}".py >/dev/null 2>&1 & python $HOME/gr-somac/apps/ping.py "${IDS_1[$ARG_ID]}" "${IDS_2[$ARG_ID]}");
+fi
+# }}}
+
+# Copy logs {{{
+cp /tmp/*npy $DIR;
+cp /tmp/*log $DIR;
+# }}}
