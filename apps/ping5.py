ceffc1148359f852c5778409c147e26f0eefd826
André Gomes <andre.gomes@dcc.ufmg.br>
2018-11-05 14:05:37 -0200

    checkpoint

--- apps/ping5.py
+++ apps/ping5.py
@@ -29,7 +29,8 @@ sleep(sleep_time)
 run_time = slots[arg_id, 1] - slots[arg_id, 0]
 print("Running time = {} min".format(run_time / 60))
 f = open("/dev/null", "w")
-call(["sudo", "timeout", str(int(run_time)), "ping", "-i 0.01", "192.168.123.1"], stdout = f)
+if run_time > 0:
+	call(["sudo", "timeout", str(int(run_time)), "ping", "-i 0.01", "192.168.123.1"], stdout = f)
 
 ####################
 # Make sure ping is dead
