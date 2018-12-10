ad933bce1fbc4ab8102266b0c51e6671683faa2b
André Gomes <andre.gomes@dcc.ufmg.br>
2018-12-10 10:30:08 -0200

    del build

--- build/include/somac/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andre/GnuRadio-SOMAC/gr-somac/include/somac
-# Build directory: /home/andre/GnuRadio-SOMAC/gr-somac/build/include/somac
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
